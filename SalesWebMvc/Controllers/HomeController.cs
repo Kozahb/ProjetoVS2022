e4ea3673fc689e3b6a519c860d0c4294eb9775c5
Thaleson Eduardo <thalison1230@gmail.com>
2024-12-27 22:04:47 -0300

    First controller and Razor pages tests

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -20,7 +20,7 @@ namespace SalesWebMvc.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Salles Web MVC App from C# Course";
             ViewData["Email"] = "engthaleson@gmail.com";
             return View();
 
