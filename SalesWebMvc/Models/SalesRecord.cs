edb8a0dc6a6dc8a0648f5f667f088a0edfa47989
Thaleson Eduardo <thalison1230@gmail.com>
2025-01-07 20:13:16 -0300

    SellersController

--- SalesWebMvc/Models/SalesRecord.cs
+++ SalesWebMvc/Models/SalesRecord.cs
@@ -9,13 +9,13 @@ namespace SalesWebMvc.Models
         public int Id { get; set; }
         public DateTime Date { get; set; }
         public double Amout { get; set; }
-        public SalesStatus Status { get; set; }
+        public SaleStatus Status { get; set; }
         public Seller Saller { get; set; }
 
         public SalesRecord() { 
         }
 
-        public SalesRecord(int id, DateTime date, double amout, SalesStatus status, Seller saller)
+        public SalesRecord(int id, DateTime date, double amout, SaleStatus status, Seller saller)
         {
             Id = id;
             Date = date;
